3ee4a2ddb711b7315b564666a79e027e04649c62
avicomputersolutions <30495311+avicomputersolutions@users.noreply.github.com>
2024-12-09 11:56:18 +0000

    Operators

---
+++ 01BasicOfJava/ArithmaticsOperator.java
@@ -0,0 +1,36 @@
+public class ArithmaticsOperator {
+    public static void main(String[] args) {
+        // Sum = a+b // + is operator and rest symbol written are variable
+        // LHS =RHS // RHS side is generally take as input
+        // LHS is output
+
+        // Flow of Program
+
+        // Variable = Input
+        int a=234567;
+        int b=2345678;
+
+        // Process 
+        int sum= a+b;
+        int Multiply= a*b;
+        int substraction= a-b;
+        int divide= b/a; // Quotient
+        int modulus= b%a; // reminder
+
+        // Output
+
+        System.out.println(sum);
+        System.out.println(Multiply);
+        System.out.println(substraction);
+
+
+        // Why we are using ArithmaticsOperator?
+        // Arithmatic is used to solve expression( BODMAS equation)
+
+
+
+
+
+    }
+    
+}
---
+++ condtionalStatement/ConditionOperator.java
@@ -0,0 +1,24 @@
+package condtionalStatement;
+
+public class ConditionOperator {
+    public static void main(String[] args) {
+        // condtion operator is used to decide that the equation is true or false
+        // Like LHS = RHS here = is condtion operator in mathematics
+        // but in language 
+        // > - Greater than
+        // < - Less than
+        // >= Greater than equal to
+        // <= Less than equal to
+        // == - Equal to // Its similar to = in mathematics
+        // !=  - Not Equal
+
+        int a =45;
+        int b =67;
+
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a==b);
+
+    }
+    
+}
