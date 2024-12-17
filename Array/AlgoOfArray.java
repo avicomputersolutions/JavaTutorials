228948794aedff5ce866266d34e63eb6ad014225
avicomputersolutions <30495311+avicomputersolutions@users.noreply.github.com>
2024-12-17 12:10:52 +0000

    Update Array

--- Array/AlgoOfArray.java
+++ Array/AlgoOfArray.java
@@ -18,6 +18,12 @@ public class AlgoOfArray {
                 arr[i]=sc.nextInt();
             }
     }
+    static void UpdateArray(int[] arr,int pos,int value){
+            if(pos<0 || pos>arr.length){        // Checking Overflow and Underflow
+                System.out.println("Position out of range");
+            }
+            arr[pos-1]=value;
+    }
 
 
 
@@ -27,8 +33,12 @@ public class AlgoOfArray {
     //    // AlgoOfArray ag=new AlgoOfArray();
     //     printArray(a);
 
-        int[] arr= new int[5];
-        insertInArray(arr);
-        printArray(arr);
+        // int[] arr= new int[5];
+        // insertInArray(arr);
+        // printArray(arr);
+
+        int[] a={1,2,1,3,2,9,8,7,4,5};
+        UpdateArray(a, 4, 78);
+        printArray(a);
     }
 }
