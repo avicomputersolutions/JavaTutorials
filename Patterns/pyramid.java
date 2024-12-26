ec9117a4755a16d5fa350b11842ad8c7d0ce3d44
avicomputersolutions <30495311+avicomputersolutions@users.noreply.github.com>
2024-12-26 13:14:23 +0000

    pyramid

---
+++ Patterns/pyramid.java
@@ -0,0 +1,17 @@
+package Patterns;
+
+public class pyramid {
+    public static void main(String[] args) {
+        int n=5;
+        for(int r=1;r<=n;r++){
+            for(int sp=1;sp<=n-r; sp++){
+                System.out.print(" "+"");
+            }
+            for(int c=1;c<=r;c++){
+                System.out.print("*"+" ");
+            }
+
+            System.out.println();
+        }
+    }
+}
